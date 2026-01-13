c19b3ed77ead29f0f840a5e8c4ad54d666fa827b
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2026-01-13 07:49:18 +0700

    feat(api): implement CRUD operations for visits

---
+++ src/app/api/visits/[visit_id]/route.ts
@@ -0,0 +1,96 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+import { getInclude } from "@/utils/prismaQuery";
+
+type Params = {
+    params: {
+        visit_id: string;
+    };
+};
+
+export async function GET(req: Request, { params }: Params) {
+    try {
+        const { visit_id } = await params;
+
+        const searchParams = new URL(req.url).searchParams;
+        const include = getInclude(searchParams, [
+            "patient",
+            "visitDetails",
+            "drugUsages",
+            "incomes",
+        ]);
+
+        const visit = await prisma.visit.findUnique({
+            where: {
+                visit_id,
+            },
+            include,
+        });
+
+        if (!visit) {
+            return NextResponse.json(
+                { message: "ไม่พบข้อมูลผู้ป่วย" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({ data: visit });
+    } catch (error: any) {
+        console.error("Get visit by id error:", error);
+        return NextResponse.json(
+            { message: "เกิดข้อผิดพลาดภายในเซิร์ฟเวอร์", error: error.message },
+            { status: 500 }
+        );
+    }
+}
+
+export async function PUT(req: Request, { params }: Params) {
+    try {
+        const { visit_id } = await params;
+        const body = await req.json();
+
+        const Updatevisit = await prisma.visit.update({
+            where: {
+                visit_id,
+            },
+            data: {
+                visit_date: body.visit_date,
+                symptom: body.symptom,
+                diagnosis: body.diagnosis,
+                note: body.note,
+            },
+        });
+
+        return NextResponse.json({ data: Updatevisit });
+    } catch (error: any) {
+        console.error("Update visit by id error: ", error);
+        return NextResponse.json(
+            { message: "เกิดข้อผิดพลาดภายในเซิร์ฟเวอร์", error: error.message },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(req: Request, { params }: Params) {
+    try {
+        const { visit_id } = await params;
+
+        const deletedvisit = await prisma.visit.update({
+            where: {
+                visit_id,
+            },
+            data: {
+                deleted_at: new Date(),
+            },
+        });
+
+        return NextResponse.json({ data: deletedvisit });
+    } catch (error: any) {
+        console.error("Delete visit by id error: ", error);
+        return NextResponse.json(
+            { message: "เกิดข้อผิดพลาดภายในเซิร์ฟเวอร์", error: error.message },
+            { status: 500 }
+        );
+    }
+}
---
+++ src/app/api/visits/route.ts
@@ -0,0 +1,73 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+import { getPagination } from "@/utils/pagination";
+import { getOrderBy, getInclude } from "@/utils/prismaQuery";
+
+export async function GET(req: Request) {
+    try {
+        const searchParams = new URL(req.url).searchParams;
+
+        const { page, pageSize, skip, take } = getPagination(searchParams);
+
+        const orderBy = getOrderBy(searchParams);
+        const include = getInclude(searchParams, [
+            "patient",
+            "visitDetails",
+            "drugUsages",
+            "incomes",
+        ]);
+
+        const visit = await prisma.visit.findMany({
+            skip,
+            take,
+            orderBy,
+            include,
+            where: { deleted_at: null },
+        });
+
+        const total = await prisma.visit.count({
+            where: { deleted_at: null },
+        });
+
+        const pageCount = Math.ceil(total / pageSize);
+
+        return NextResponse.json({
+            data: visit,
+            meta: {
+                pagination: {
+                    page,
+                    pageSize,
+                    pageCount,
+                    total,
+                },
+            },
+        });
+    } catch (error: any) {
+        console.error("Register error:", error);
+        return NextResponse.json(
+            { message: "เกิดข้อผิดพลาดภายในเซิร์ฟเวอร์", error: error.message },
+            { status: 500 }
+        );
+    }
+}
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+
+        const visit = await prisma.visit.create({
+            data: {
+                patient_id: body.patient_id,
+                visit_date: body.visit_date,
+                symptom: body.symptom,
+                diagnosis: body.diagnosis,
+                note: body.note,
+            },
+        });
+
+        return NextResponse.json(visit, { status: 201 });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
