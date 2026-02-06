b70960ccc9f1ecd881befb3f936b924611421973
lillianxhub <petkun222@gmail.com>
2026-02-07 02:35:11 +0700

    feat: Implement patient search functionality, add medicine creation API, and introduce a `useDebounce` hook.

--- src/interface/patient.ts
+++ src/interface/patient.ts
@@ -1,34 +1,34 @@
 import { Gender } from "@/constants/gender";
 
 export interface Patient {
-  id: string;
-  hospital_number: string;
-  fullName: string;
-  gender: Gender;
-  birthDate: string;
-  phone: string;
-  address?: string;
-  allergy?: string;
+    patient_id: string;
+    hospital_number: string;
+    fullName: string;
+    gender: Gender;
+    birthDate: string;
+    phone: string;
+    address?: string;
+    allergy?: string;
 }
 
 export interface CreatePatientPayload {
-  first_name: string;
-  last_name: string;
-  gender: Gender;
-  birth_date?: string;
-  phone?: string;
-  address?: string;
-  allergy?: string;
+    first_name: string;
+    last_name: string;
+    gender: Gender;
+    birth_date?: string;
+    phone?: string;
+    address?: string;
+    allergy?: string;
 }
 
 export interface PatientApiResponse {
-  patient_id: string;
-  hospital_number: string;
-  first_name: string;
-  last_name: string;
-  gender: Gender;
-  birth_date: string;
-  phone: string;
-  address?: string;
-  allergy: string;
+    patient_id: string;
+    hospital_number: string;
+    first_name: string;
+    last_name: string;
+    gender: Gender;
+    birth_date: string;
+    phone: string;
+    address?: string;
+    allergy: string;
 }
